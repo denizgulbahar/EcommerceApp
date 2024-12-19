7dccb4f50126a2ac01041954b8dd96fbba5f1bc8
deniz <denizgulbahar96@gmail.com>
2024-12-19 06:02:01 +0300

    Fixed removing context issue.

--- navigators/mainStack/childs/MainStackLoggedIn.js
+++ navigators/mainStack/childs/MainStackLoggedIn.js
@@ -4,11 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LanguageButton from '../../../components/buttons/languageButton'; 
 import ClientBottomNavigator from '../../bottomNavigator/ClientBottomNavigator'; 
 import MyAccountScreen from '../../../screens/clientScreens/myAccountScreens/MyAccountScreen';
-import PersonalInformationScreen from '../../../screens/clientScreens/myAccountScreens/PersonalInformationScreen'; 
-import PreviousOrderScreen from '../../../screens/clientScreens/myAccountScreens/PreviousOrderScreen'; 
 import SpecificCategoryScreen from '../../../screens/clientScreens/homeScreens/SpecificCategoryScreen';
 import ProductDetailScreen from '../../../screens/clientScreens/homeScreens/ProductDetailScreen'; 
-import ShoppingCartScreen from '../../../screens/clientScreens/ShoppingCartScreen'; 
 import OtherCategoriesScreen from '../../../screens/clientScreens/homeScreens/OtherCategoriesScreen';
 import FavouriteScreen from '../../../screens/clientScreens/FavouriteScreen'; 
 
@@ -40,8 +37,8 @@ function MainStackLoggedIn() {
       >
         <Stack.Screen name="client" component={ClientBottomNavigator} />
         <Stack.Screen name="my-account" component={MyAccountScreen} />
-        <Stack.Screen name="personal-informations" component={PersonalInformationScreen} />
-        <Stack.Screen name="previous-orders" component={PreviousOrderScreen} />
+        {/* <Stack.Screen name="personal-informations" component={PersonalInformationScreen} />
+        <Stack.Screen name="previous-orders" component={PreviousOrderScreen} /> */}
         <Stack.Screen name="home" component={SpecificCategoryScreen} />
         <Stack.Screen name="details" component={ProductDetailScreen} />
         <Stack.Screen name="other-categories" component={OtherCategoriesScreen} />
