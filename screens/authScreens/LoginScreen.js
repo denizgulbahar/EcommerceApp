acd6a87eb57510f3e40682d2f28d4a0664ffcf3e
deniz <denizgulbahar96@gmail.com>
2025-01-22 14:07:34 +0300

    Made some changes in login module.

--- screens/authScreens/LoginScreen.js
+++ screens/authScreens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Image, Alert } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import ButtonOriginal from '../../components/buttons/buttonOriginal';
 import InputOriginal from '../../components/input/inputOriginal';
 import { Header } from '../../components/header/header';
@@ -8,51 +8,55 @@ import LoginButton from '../../redux/feature/auth/loginButton';
 import { useTranslation } from 'react-i18next';
 import withLoadingLogin from '../../utilities/withLoadingLogin';
 
-const LoginScreen = ({ navigation, updateLoading }) => {
+const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState("deniz.gulbahar@gmail.com");
     const [password, setPassword] = useState('Ecommerce123');
     const { t } = useTranslation()
     const handleForgotPassword = () => {
         navigation.navigate('password');
     };
+    const LoginComponents = ({ updateLoading }) => (
+    <>
+        <Image
+            source={require('../../assets/logo/logo.png')}
+            style={[styles.logo]}
+            resizeMode="contain"
+        />      
+        <Header text={t("welcomeApp")} />
+        <InputOriginal
+            icon="email"
+            rightIcon="checkbox-marked-circle"
+            placeholder={t("email")}
+            value={email}
+            onChangeText={setEmail}
+        />
+        <InputOriginal
+            icon="lock"
+            placeholder={t("password")}
+            kind="password"
+            keyboard="password"
+            secureText="true"
+            value={password}
+            onChangeText={setPassword}
+        />
+        <View style={{ alignItems: "flex-end" }}>
+            <ButtonOriginal
+                title={t("forgotPassword")}
+                onPress={handleForgotPassword}
+            />
+        </View>
+            <LoginButton 
+                email={email}
+                password={password}
+                title={t("login")}
+                updateLoading={updateLoading} 
+            />
+    </> )
+    // In real API call, show loading component while waiting for the response
+    const LoginWithLoading = withLoadingLogin(LoginComponents)
     return (
         <ScreenWrapper>
-            <>
-                <Image
-                    source={require('../../assets/logo/logo.png')}
-                    style={[styles.logo]}
-                    resizeMode="contain"
-                />      
-                <Header text={t("welcomeApp")} />
-                <InputOriginal
-                    icon="email"
-                    rightIcon="checkbox-marked-circle"
-                    placeholder={t("email")}
-                    value={email}
-                    onChangeText={setEmail}
-                />
-                <InputOriginal
-                    icon="lock"
-                    placeholder={t("password")}
-                    kind="password"
-                    keyboard="password"
-                    secureText="true"
-                    value={password}
-                    onChangeText={setPassword}
-                />
-                <View style={{ alignItems: "flex-end" }}>
-                    <ButtonOriginal
-                        title={t("forgotPassword")}
-                        onPress={handleForgotPassword}
-                    />
-                </View>
-                    <LoginButton 
-                        email={email}
-                        password={password}
-                        title={t("login")}
-                        updateLoading={updateLoading} 
-                    />
-            </>
+           <LoginWithLoading />
         </ScreenWrapper>
     );
 };
@@ -74,7 +78,6 @@ const styles = StyleSheet.create({
     }
 
 });
-// In real API call, show loading component while waiting for the response
-const LoginWithLoading = withLoadingLogin(LoginScreen)
 
-export default LoginWithLoading;
+
+export default LoginScreen;
